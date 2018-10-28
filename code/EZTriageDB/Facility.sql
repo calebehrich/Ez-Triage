2ffc23cfc3e372be8a4a572c74e2e3dccbe557af
caleb <cehrich02@gmail.com>
2018-10-28 18:29:05 -0400

    Added Patient DB, Added classes

---
+++ code/EZTriageDB/Facility.sql
@@ -0,0 +1,5 @@
+﻿CREATE STATISTICS [Facility]
+	ON [dbo].[SomeTableOrView]
+		(SomeColumn)
+	WITH
+	SAMPLE 50 PERCENT
---
+++ code/EZTriageDB/Patient.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Patient]
+(
+	[FirstResponderID] int identity (1, 1) not null,
+	[PatientID] int not null,
+	[PatientLastName] nvarchar(50) null,
+	[PatientFirstName] nvarchar(50) null,
+	[PatientPhoneNumber] int null,
+	[PatientAddress] nvarchar(50) null,
+	[PatientTriageCat] nvarchar(20) null,
+	[Date] datetime null,
+	primary key clustered ([FirstResponderID] asc)
+)
