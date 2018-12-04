dc94248a6007de2c4f16c218edc9409b372516d8
caleb <cehrich02@gmail.com>
2018-12-03 20:07:16 -0500

    Change Button Layout in Dashboard

---
+++ code/EzTriage/Controllers/EncounterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EzTriage.Controllers
+{
+    public class EncounterController : Controller
+    {
+        // GET: Encounter
+        public ActionResult NewEncounter()
+        {
+            return RedirectToAction("NewEncounter", "Encounter");
+        }
+    }
+}
