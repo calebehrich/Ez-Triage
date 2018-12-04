e7c0eae385278f3fa3a49301276a82968905cfa1
caleb <cehrich02@gmail.com>
2018-12-03 19:39:40 -0500

    Fixed issue with DB. added buttons to dashboard

--- code/EzTriage/Controllers/HomeController.cs
+++ code/EzTriage/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace EzTriage.Controllers
 {
-    public class DashBoardController : Controller
+    public class HomeController : Controller
     {
         public ActionResult Index()
         {
---
+++ code/EzTriageDB/dbo/Tables/UserValidation.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[UserValidation] (
+    [EmployeeID] INT           NOT NULL,
+    [Username]   NVARCHAR (50) NULL,
+    [Password]   NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([EmployeeID] ASC)
+);
+
